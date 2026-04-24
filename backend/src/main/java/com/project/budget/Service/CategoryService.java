3729004f5c771af5116750dde0178492ca273f00
Apollyon12346 <zhengjason248@gmail.com>
2026-04-24 15:47:52 -0400

    Add files via upload

--- backend/src/main/java/com/project/budget/Service/CategoryService.java
+++ backend/src/main/java/com/project/budget/Service/CategoryService.java
@@ -1,12 +1,16 @@
 package com.project.budget.Service;
 
 import com.project.budget.domain.CreateCategoryRequest;
+import com.project.budget.domain.UpdateCategoryRequest;
 import com.project.budget.domain.entity.BudgetCategory;
 
 import java.util.List;
+import java.util.UUID;
 
 //create the interface for the creating task method
 public interface CategoryService {
     BudgetCategory createCategory(CreateCategoryRequest request);
     List<BudgetCategory> listCategories();
+    BudgetCategory updateCategory(UUID categoryid, UpdateCategoryRequest request);
+    void deleteCategory(UUID categoryid);
 }
